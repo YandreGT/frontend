384d4cf2b755a3e2948df6ce14697d3cea8c1d85
Yandre Galindo <yandre@cinemedios.com>
2023-06-06 02:59:58 -0600

    Corregido TodoItem ⚛

---
+++ 03-todo-list/src/components/TodoItem.jsx
@@ -0,0 +1,13 @@
+const TodoItem = ({todo, handleDelete}) => {
+  return (
+    <li>{todo}
+        <button
+            className='delete-button'
+            onClick={handleDelete}
+        >Eliminar
+        </button>
+    </li>
+  )
+}
+
+export default TodoItem
--- 03-todo-list/src/components/TodoList.jsx
+++ 03-todo-list/src/components/TodoList.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react"
+import TodoItem from "./TodoItem"
+
 const TodoList = () => {
-  return (
-    <div>Lista de Tareas</div>
-  )
+    /*Estado para manejar el input para agregar una tarea*/
+    const [inputValue, setInputValue] = useState('')
+
+    /* Estado para almacenar la lista de tareas que voy agregando en un arreglo */
+    const [todos, setTodos] = useState([])
+
+    /* Función que agrega tareas a mi lista */
+    const handleAddItem = () => {
+        if(inputValue.trim()){//.trim() Ayuda a limpiar espacios en blanco, al inicio y al final de la cadena
+            setTodos([...todos, inputValue])
+            setInputValue('')
+            console.log(todos)
+    }
 }
 
+const handleDelete = (index) => {
+    setTodos(todos.filter((item, i) => i !== index))
+}
+    return (
+    <div>
+        <h1>Lista de Tareas</h1>
+        {/* Para tomar control de la información que hay en input, podemos hacer uso del evento onChange para guardar la información del input apenas sea tecleado y guardarla en mi estado */}
+        <input type='text' value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+        <button onClick={handleAddItem}>Agregar</button>
+        <ul>
+            {todos.map((item, index) => (
+            <TodoItem
+                todo={item}
+                handleDelete={() => handleDelete(index)}
+                key={index}
+            />
+        ))}
+     </ul>
+    </div>
+  )
+}
 export default TodoList

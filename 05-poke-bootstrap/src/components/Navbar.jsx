691310a0e28129447ef9fcc0567f2e5fc1bb6bfe
Yandre Galindo <yandre@cinemedios.com>
2023-06-14 23:05:49 -0600

    ✨Actualizada la Navbar

--- 05-poke-bootstrap/src/components/Navbar.jsx
+++ 05-poke-bootstrap/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
-    return (
-      <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
-        <a className='navbar-brand' href='#'>Pokédex</a>
-  
-        <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
-          <li className='nav-item px-2'>
-            <a className='nav-link' href='/'>Home</a>
-          </li>
-          <li className='nav-item px-2'>
-            <a className='nav-link' href='/about'>Acerca de Pokémon</a>
-          </li>
-        </ul>
-  
-      </nav>
-    )
-  }
-  export default Navbar
+  return (
+    <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
+      <NavLink className='navbar-brand' to='#'>Pokédex</NavLink>
+
+      <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+        <li className='nav-item px-2'>
+          <NavLink className='nav-link' to='/'>Home</NavLink>
+        </li>
+        <li className='nav-item px-2'>
+          <NavLink className='nav-link' to='/about'>Acerca de Pokémon</NavLink>
+        </li>
+      </ul>
+
+    </nav>
+  )
+}
+export default Navbar
